722fb19cbfaed9e32a1285bac55db95df5517071
ssaaach <sachinbhat12781@gmail.com>
2025-04-05 17:13:13 +0530

    loanController.js coded

---
+++ loan-network/backend/controllers/loanController.js
@@ -0,0 +1,23 @@
+const { loadLoans, saveLoans } = require('../config/db');
+
+exports.requestLoan = (req, res) => {
+  const { userId, amount, reason } = req.body;
+  let loans = loadLoans();
+
+  const newLoan = {
+    id: Date.now(),
+    userId,
+    amount,
+    reason,
+    status: 'pending',
+    timestamp: new Date()
+  };
+  loans.push(newLoan);
+  saveLoans(loans);
+  res.status(201).json({ msg: 'Loan request submitted', loan: newLoan });
+};
+
+exports.getAllLoans = (req, res) => {
+  const loans = loadLoans();
+  res.json(loans);
+};
