ce21b02f98d5644db4a2f53bc1e2abd578069607
ssaaach <sachinbhat12781@gmail.com>
2025-04-06 02:26:22 +0530

    ufb

--- loan-network/backend/routes/loanRoutes.js
+++ loan-network/backend/routes/loanRoutes.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {
-  requestLoan,
-  offerLoan,
-  getLoans,
-  repayLoan
-} = require('../controllers/loanController');
+const { requestLoan, getAllLoans } = require('../controllers/loanController');
 
 // @route   POST /api/loans/request
 router.post('/request', requestLoan);
 
-// @route   POST /api/loans/offer
-router.post('/offer', offerLoan);
-
 // @route   GET /api/loans
-router.get('/', getLoans);
-
-// @route   POST /api/loans/repay
-router.post('/repay', repayLoan);
+router.get('/', getAllLoans);
 
 module.exports = router;
