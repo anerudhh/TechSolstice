0d6d0736228edddf45ce05f3e08a905f6e00f6da
ssaaach <sachinbhat12781@gmail.com>
2025-04-06 02:15:07 +0530

    waroutes

--- loan-network/backend/routes/whatsappRoutes.js
+++ loan-network/backend/routes/whatsappRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { sendWhatsAppMessage } = require('../controllers/whatsappController');
+const { sendWhatsAppMessage, receiveMessage } = require('../controllers/whatsappController');
 
 // @route   POST /api/whatsapp/send
 router.post('/send', sendWhatsAppMessage);
+
+// @route   POST /api/whatsapp/receive
+router.post('/receive', receiveMessage);
 
 module.exports = router;
