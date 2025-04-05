41c1fb43d41ad2b136ccfef168cabede6af2f53a
Sachin Bhat <100529554+ssaaach@users.noreply.github.com>
2025-04-05 17:32:53 +0530

    Update userRoutes.js
    userRoutes coded

---
+++ loan-network/backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginWithOtp } = require('../controllers/userController');
+
+// @route   POST /api/users/register
+router.post('/register', registerUser);
+
+// @route   POST /api/users/login
+router.post('/login', loginWithOtp);
+
+module.exports = router;
