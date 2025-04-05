3f971f2ab23df7e61030b03407018077882c96f2
ssaaach <sachinbhat12781@gmail.com>
2025-04-05 17:42:02 +0530

    blockchainRoutes.js coded

---
+++ loan-network/backend/routes/blockchainRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getLedger,
+  addLedgerEntry
+} = require('../controllers/blockchainController');
+
+// @route   GET /api/ledger
+router.get('/', getLedger);
+
+// @route   POST /api/ledger/add
+router.post('/add', addLedgerEntry);
+
+module.exports = router;
